df99c433dfc0016f62c87988126af6c15cfc6f93
Aaron Miller <millerac333@gmail.com>
2018-07-10 13:33:28 -0500

    added more files

--- src/scripts/eventListenters.js
+++ src/scripts/eventListenters.js
@@ -1,4 +1,9 @@
 const $ = require("jquery");
 const databaseMethods = require("./databaseMethods");
-const mainMovieDashboard = require("./homeBuild")
+const mainMovieDashboard = require("./homeBuild");
+const clear = require("./clear")
 
+const body = document.querySelector("body");
+body.addEventListener("click", () => {
+    if (event.target.id === "addMovieButton") {
+        clear();
