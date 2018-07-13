dfe0a07ed2fdd0def2e85797da01f413a100c289
Aaron Miller <millerac333@gmail.com>
2018-07-12 19:09:39 -0500

    added to databaseMethods.js

--- src/scripts/databaseMethods.js
+++ src/scripts/databaseMethods.js
@@ -1,28 +1,54 @@
 const $ = require("jquery");
 
-const databaseMethods = Object.create({}, {
+const databaseMethods = Object.create(
+  {},
+  {
     postMovie: {
-        value: (movies) => {
-            return $.ajax({
-                url: "http://localhost:3000/movies",
-                method: "POST",
-                data: movies
-            })
-        }
+      value: movies => {
+        return $.ajax({
+          url: "http://localhost:3000/movies",
+          method: "POST",
+          data: movies
+        });
+      }
     },
     getAllMovies: {
-        value: () => {
-            return $.ajax("http://localhost:3000/movies")
-        }
+      value: () => {
+        return $.ajax("http://localhost:3000/movies");
+      }
+    },
+    getMovie: {
+      value: id => {
+        return $.ajax({
+          url: `http://localhost:3000/movies/${id}`,
+          method: "GET"
+        });
+      }
+    },
+    putMovie: {
+      value: editMovie => {
+        return $.ajax({
+          url: `http://localhost:3000/movies/${editMovie.id}`,
+          method: "PUT",
+          data: {
+            title: editMovie.title,
+            plot: editMovie.plot,
+            duration: editMovie.duration,
+            watched: editMovie.watched,
+            id: editMovie.id
+          }
+        });
+      }
     },
     deleteMovie: {
-        value: (id) => {
-            return $.ajax({
-                url: `http://localhost:3000/movies/${id}`,
-                method: "DELETE"
-            })
-        }
+      value: id => {
+        return $.ajax({
+          url: `http://localhost:3000/movies/${id}`,
+          method: "DELETE"
+        });
+      }
     }
-})
+  }
+);
 
 module.exports = databaseMethods;
